845b5b2c23a39b04989f40712770740ab0e6acda
Denis Popkov <d.popkov@ltst.ru>
2023-10-29 11:57:28 +0600

    add: reviewer component

---
+++ app/src/main/java/ru/popkov/gameapp/screens/game/components/ReviewerComponent.kt
@@ -0,0 +1,88 @@
+package ru.popkov.gameapp.screens.game.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.popkov.gameapp.R
+import ru.popkov.gameapp.common.TextComponent
+import ru.popkov.gameapp.data.models.ReviewerModel
+import ru.popkov.gameapp.ui.theme.DividerColor
+import ru.popkov.gameapp.ui.theme.ModernistTextRegular12
+import ru.popkov.gameapp.ui.theme.ModernistTextRegular16
+import ru.popkov.gameapp.ui.theme.SecondTextColor
+import ru.popkov.gameapp.ui.theme.ThirdTextColor
+
+@Composable
+fun ReviewerComponent(
+    modifier: Modifier = Modifier,
+    reviewer: ReviewerModel,
+    isShouldShowDivider: Boolean = true
+) {
+    Column(modifier = modifier.padding(horizontal = 24.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(space = 16.dp)
+        ) {
+            ReviewerImageComponent(
+                modifier = modifier,
+                reviewerImage = reviewer.reviewerImage
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(space = 7.dp)
+            ) {
+                TextComponent(
+                    modifier = modifier,
+                    text = reviewer.reviewerName,
+                    textColor = Color.White,
+                    lineHeight = 20.sp,
+                    textStyle = ModernistTextRegular16
+                )
+                TextComponent(
+                    modifier = modifier,
+                    text = reviewer.reviewDatePublished,
+                    textColor = ThirdTextColor,
+                    lineHeight = 15.sp,
+                    textStyle = ModernistTextRegular12
+                )
+            }
+        }
+        Spacer(modifier = modifier.padding(top = 16.dp))
+        TextComponent(
+            modifier = modifier,
+            text = reviewer.reviewComment,
+            textColor = SecondTextColor,
+            lineHeight = 20.sp,
+            textStyle = ModernistTextRegular12
+        )
+    }
+
+    AnimatedVisibility(visible = isShouldShowDivider) {
+        Divider(
+            modifier = modifier.padding(horizontal = 38.dp, vertical = 24.dp),
+            thickness = 1.dp,
+            color = DividerColor
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 1)
+@Composable
+fun ReviewerComponentPreview() {
+    ReviewerComponent(
+        reviewer = ReviewerModel(
+            reviewerName = R.string.first_reviewer_name,
+            reviewDatePublished = R.string.first_review_date,
+            reviewComment = R.string.first_reviewer_comment,
+            reviewerImage = R.drawable.ic_first_reviewer
+        )
+    )
+}
